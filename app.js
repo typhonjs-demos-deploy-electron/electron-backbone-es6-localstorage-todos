e76546b96e8aa39c8ef8ff9570e87fa9936c292d
typhonrt <support@typhonrt.org>
2016-01-27 14:14:02 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * index.js -- Provides a minimal Electron app launching the bundled version of `backbone-es6-localstorage-todos`.
+ *
+ * Please see the JSPM override for `backbone-es6-localstorage-todos` defined in `package.json` that limits files
+ * installed by JSPM to the minimum necessary to launch the bundled app.
+ */
+
+var app =            require('app');
+var BrowserWindow =  require('browser-window');
+
+app.on('ready', function()
+{
+   var mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+   mainWindow.loadURL('file://' + __dirname
+    + '/jspm_packages/github/typhonjs-demos/backbone-es6-localstorage-todos@master/index.html');
+});
